2f2f0cafe237ab0fa87cad1b5cfaef4a10d2222e
Eryck Assis <eng.assis.dev@gmail.com>
2025-06-02 21:29:07 -0300

    refactor: update project structure and improve server.js routing

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 import http from "http";
 
+const PORT = 3000;
+
+const rotas = {
+  "/": "Curso de Node.js",
+  "/Livros": "Livros de Node.js",
+  "/autores": "Autores de Node.js",
+};
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Curso de Node.js");
+  res.end(rotas[req.url]);
 });
 
-server.listen(3000, () => {
+server.listen(PORT, () => {
   console.log("Servidor escutando na porta 3000");
 });
-// Para executar este código, salve-o em um arquivo chamado server.js e execute o comando:
